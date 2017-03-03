3da32110a7e5c36c3ebf210a48c7c1f871fe90e3
Sylvia <sylviad1221@gmail.com>
2017-03-02 20:14:57 -0500

    add render page /w react router and main component w/ actions/state binding to props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 // Codegram router/render file
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// import styles
+
+// Import components
+import App from './components/App';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// Import react router dependencies
+import { Router, Route, IndexRoute, borwserHistory } from 'react-router';
+
+const router = (
+  <Provider>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+render(router, document.getElementById('root'));
